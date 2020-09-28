b46191965977ea958df1269c12d1ef82ba14a6e9
Cameron <alodith15@gmail.com>
2020-09-28 19:54:17 +0100

    Staged New 3D Rhythm Game Files
    Gyroscoping sorted

---
+++ Mobile Beat Game 3D/Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public float minRotation;
+    public float maxRotation;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        
+        dir.z = Mathf.Clamp(dir.z, minRotation, maxRotation);
+        dir.z -= Input.acceleration.x;
+
+
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        transform.Rotate(dir);
+    }
+}
---
+++ Mobile Beat Game 3D/Assets/Scripts/GyroControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    public bool gyroIsEnabled;
+
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
+    private void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        gyroEnabled = EnableGyro();
+        gyroIsEnabled = false;
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            gyroIsEnabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+            rot = new Quaternion(0, 0, 1, 0);
+            return true;
+            
+        }
+
+        return false;
+    }
+
+    private void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+        }
+    }
+}
